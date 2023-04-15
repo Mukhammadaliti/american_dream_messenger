27f38a8025eb6e1e7a4aa078c263d8959c5a3cca
Mukhammadaliti <mukhammadaliti02@gmail.com>
2023-04-16 01:06:23 +0600

    add welcom moduls

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,9 +4,11 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const WELCOM = _Paths.WELCOM;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const WELCOM = '/welcom';
 }
