04ed78250269ac426d2ee473380d3c3ebf18d012
Mukhammadaliti <mukhammadaliti02@gmail.com>
2023-04-27 17:26:57 +0600

    add fire base

--- lib/app/modules/log_in/views/log_in_view.dart
+++ lib/app/modules/log_in/views/log_in_view.dart
@@ -1,4 +1,6 @@
 import 'package:american_dream_messenger/app/constans/app_colors.dart';
+import 'package:american_dream_messenger/app/modules/log_in/controllers/log_in_controller.dart';
+import 'package:american_dream_messenger/app/modules/log_in/widgets/sing_in_text_form_filed.dart';
 import 'package:american_dream_messenger/app/modules/opt/views/opt_view.dart';
 import 'package:american_dream_messenger/app/modules/sing_up/views/sing_in_view.dart';
 import 'package:american_dream_messenger/app/modules/welcom/views/welcom_view.dart';
@@ -9,57 +11,46 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
-import '../controllers/log_in_controller.dart';
+// import '../controllers/log_in_controller.dart';
 
 class LogInView extends GetView<LogInController> {
   const LogInView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          AppBarWidget(text: "Login", textt: """Enter your 10 - digit mobile to 
-continue."""),
-          SizedBox(
-            height: 180,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextFormField(
-              style: TextStyle(decorationColor: AppColors.red),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: AppColors.transparent,
-                focusColor: AppColors.blueB8,
-                labelText: "Phone",
-                hintText: "Enter your number",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide: BorderSide(color: AppColors.blue, width: 2.0),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide: BorderSide(color: AppColors.blueB8, width: 1.0),
-                ),
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            AppBarWidget(
+                text: "Login", textt: """Enter your 10 - digit mobile to 
+      continue."""),
+            SizedBox(
+              height: 180,
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          RegisterWidget(
-            onpress: () {
-              Get.to(
-                OptView(),
-              );
-            },
-            text: "Login",
-          )
-        ],
+            SingInTextFormFiled(
+              text: "Email",
+              world: "Enter your email address",
+              onChange: (p0) {},
+            ),
+            SingInTextFormFiled(
+              text: "Password",
+              world: "Enter your password",
+              onChange: (p0) {},
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            RegisterWidget(
+              onpress: () {
+                Get.to(
+                  OptView(),
+                );
+              },
+              text: "Login",
+            )
+          ],
+        ),
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.center,
